8271f0b7c934608a40fe2abdf1eecbf622753140
Jayant Chauhan <jayantjagdishchauhan@gmail.com>
2019-06-29 16:05:33 +0530

    Added class.

---
+++ src/main/java/com/hackerrank/solutions/SolutionPlusMinus.java
@@ -0,0 +1,11 @@
+package com.hackerrank.solutions;
+
+/*
+ * Solution for this problem
+ *
+ * https://www.hackerrank.com/challenges/plus-minus/problem
+ *
+ **/
+
+public class SolutionPlusMinus {
+}
