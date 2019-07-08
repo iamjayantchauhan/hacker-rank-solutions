36f25acafcd82ef48e8c8f2041468b60dee17b36
Jayant Chauhan <jayantjagdishchauhan@gmail.com>
2019-07-08 09:45:48 +0530

    Added Solutions

---
+++ src/main/java/com/hackerrank/solutions/StairCaseSolution.java
@@ -0,0 +1,39 @@
+package com.hackerrank.solutions;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/*
+ * Solution for stair case solution
+ *
+ * https://www.hackerrank.com/challenges/staircase/problem
+ *
+ **/
+public class StairCaseSolution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(j < n - i ? " " : "#");
+            }
+            System.out.println("");
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
