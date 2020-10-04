eb1c7b1a1d09b43c316bd5ebcc2ad9f768d8a39c
Jayant Chauhan <jayantjagdishchauhan@gmail.com>
2020-10-04 10:06:30 +0530

    Player challenge

---
+++ src/main/java/com/hackerrank/solutions/PlayerComparison.java
@@ -0,0 +1,50 @@
+package com.hackerrank.solutions;
+
+import java.util.*;
+
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player> {
+    // complete this method
+    public int compare(Player a, Player b) {
+        int nameCompare = a.name.compareTo(b.name);
+
+        // Change a -> b or b -> a to change sorting order
+        int scoreCompare = b.score - a.score;
+
+        if (scoreCompare == 0) {
+            return nameCompare == 0 ? scoreCompare : nameCompare;
+        }
+        return scoreCompare;
+    }
+}
+
+
+class PlayerComparison {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for (int i = 0; i < n; i++) {
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for (int i = 0; i < player.length; i++) {
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
